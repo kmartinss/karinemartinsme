bffd721adbb29e6ded7dcd11dfcfa7dba6e26999
KarineMartins <martinskazk@gmail.com>
2021-03-24 00:17:03 -0300

    Ajustes visuais na tela "Sobre"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { AboutComponent } from './views/about/about.component';
-import { HomeComponent } from './views/home/home.component';
-import { PortfolioComponent } from './views/portfolio/portfolio.component';
-import { ContactComponent } from './views/contact/contact.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/template/header/header.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { AboutComponent } from "./views/about/about.component";
+import { HomeComponent } from "./views/home/home.component";
+import { PortfolioComponent } from "./views/portfolio/portfolio.component";
+import { ContactComponent } from "./views/contact/contact.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -18,15 +20,17 @@ import { ContactComponent } from './views/contact/contact.component';
     AboutComponent,
     HomeComponent,
     PortfolioComponent,
-    ContactComponent
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
