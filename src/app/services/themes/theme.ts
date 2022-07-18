0afb81ea89bea94ed191b2423180ac9e8688a486
Karine <martinskazk@gmail.com>
2022-07-17 21:13:22 -0300

    Configuração de variáveis scss

--- src/app/services/themes/theme.ts
+++ src/app/services/themes/theme.ts
@@ -6,27 +6,32 @@ export interface Theme {
 export const light: Theme = {
   name: "light",
   properties: {
-    "--bg-body-color": "#ffffff",
+    "--bg-body-color": "linear-gradient(90deg, #ffffff 0%, #dedede 100%)",
     "--bg-header-color": "#ffffff",
     "--main-color": "#de3132",
     "--border-color": "#de3132",
     "--menu-btn-hover": "#000",
     "--page-header": "linear-gradient(to bottom, #181818, #ffffff)",
     "--bg-card": "#ffffff",
-    "--text-color": "#000",
+    "--logo-text-color": "#000000",
+    "--text-color": "#000000",
+    "--negative-color": "#000000"
   },
 };
 
 export const dark: Theme = {
   name: "dark",
   properties: {
-    "--bg-body-color": "#1e1e1e",
+    "--bg-body-color":
+      "linear-gradient(90deg, rgb(0, 0, 0) 0%, rgb(12, 12, 12) 35%, rgb(32, 32, 32) 100%)",
     "--bg-header-color": "#000000",
     "--main-color": "#de3132",
     "--border-color": "#de3132",
     "--menu-btn-hover": "#ffffff",
     "--page-header": "linear-gradient(to bottom, #000, #1e1e1e)",
     "--bg-card": "#000000cc",
-    "--text-color": "#ffffff",
+    "--logo-text-color": "#ffffff",
+    "--text-color": "#cccccc",
+    "--negative-color": "#ffffff"
   },
 };
